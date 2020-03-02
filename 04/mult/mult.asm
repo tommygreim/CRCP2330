dccf5de07607701c0a6a6b721e19c9adbb1f4ad8
tommygreim <tommygreim1@gmail.com>
2020-03-02 10:19:46 -0600

    Might have finished mult. Assembled.

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,4 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-LOAD R1, #5;
+		@3
+		M=0
+		@2
+		M=0
+
+ (LOOP)
+ 		@3
+ 		D=M
+ 		@1
+ 		D=D-A
+ 		@END
+ 		D;JGT
+ 		@1
+ 		D=A
+ 		@2
+ 		M=D+M
+ 		@3
+ 		M=M+1
+ 		@LOOP
+ 		0;JMP
+ (END)	
+ 		@END
+ 		0;JMP
